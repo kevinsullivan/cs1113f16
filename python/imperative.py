2d62a9d50a1bd50b9c60bbd152eba57218a97e46
Kevin Sullivan <sullivan@virginia.edu>
2016-11-12 11:38:05 -0500

    Fix tabs and parens in Python example file

--- python/imperative.py
+++ python/imperative.py
@@ -36,21 +36,21 @@ print(x, y, z)
 print("Let's explore conditionals!")
 
 x = 5
-if (x == 11):					# if (boolean expr):
-	print("It's 11!")			# true branch; "..." is a string
-else:							# optional else:
-	print("It wasn't 11 ...")	# and the false branch
-	x = 11
-	print("... but now it is!")
+if x == 11:                    # if (boolean expr):
+    print("It's 11!")            # true branch; "..." is a string
+else:                            # optional else:
+    print("It wasn't 11 ...")    # and the false branch
+    x = 11
+    print("... but now it is!")
 
 
 # WHILE Commands
 
 print("Let's loop!")
-n = 5					# establish "loop index"
-while (n > 0):			# if (boolean expression)
-	print(n)			# loop body, executed if True
-	n = n - 1			# You must avoid infinite loops!!!
+n = 5                    # establish "loop index"
+while n > 0:            # if (boolean expression)
+    print(n)            # loop body, executed if True
+    n = n - 1            # You must avoid infinite loops!!!
 
 
 # Exercise: compute the sum of integers from 1 to 10
@@ -60,10 +60,10 @@ print("Let's sum 1 to n!")
 n = 10
 sum = 0
 
-while (n > 0):
-	sum = sum + n
-	n = n - 1
-	print("The sum is", sum)
+while n > 0:
+    sum = sum + n
+    n = n - 1
+    print("The sum is", sum)
 
 # Here's a version that starts at 0 and goes to 10
 
@@ -71,35 +71,33 @@ end = 10
 cur = 0
 sum = 0
 
-while (cur <= end):
-	sum = sum + cur
-	cur = cur + 1
-	print ("The sum is", sum)
+while cur <= end:
+    sum = sum + cur
+    cur = cur + 1
+    print("The sum is", sum)
 
 
 # Finally, functions and recursion
 
-def sum(n):
-	if (n == 0):
-		return 0
-	else:
-		return n + sum (n-1)
+def sum_rec(n):
+    if n == 0:
+        return 0
+    else:
+        return n + sum_rec(n-1)
 
-print ("The sum is", sum(10))
+print("The sum is", sum(10))
 
 def fac(n):
-	if (n == 0):
-		return 1
-	else:
-		return n * fac(n-1)
+    if n == 0:
+        return 1
+    else:
+        return n * fac(n-1)
 
 print(fac(100))
 
 n = 100
 fact = 1
-
-while (n > 0):
-	fact = fact * n
-	n = n - 1
-
+while n > 0:
+    fact = fact * n
+    n = n - 1
 print(fact)
